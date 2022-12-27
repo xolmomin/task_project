deacdf8405a3c5627704f3e9a15615ececeb6f7a
robiya07 <obidjonova07@mail.ru>
2022-12-27 17:15:30 +0500

    models db

--- apps/models.py
+++ apps/models.py
@@ -1,11 +1,11 @@
-# from django.db import models
-#
-# # Create your models here.
-# from django.db.models import Model, CharField, TextField
-#
-#
-# class Product(Model):
-#     name = CharField(max_length=255)
-#     description = TextField()
-#     price = CharField(max_length=255)
-#     image =
+from django.db import models
+
+# Create your models here.
+from django.db.models import Model, CharField, TextField, ImageField
+
+
+class Product(Model):
+    name = CharField(max_length=255)
+    description = TextField()
+    price = CharField(max_length=255)
+    image = ImageField(upload_to='product/')
