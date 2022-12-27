bca902aa890208655e5f512a1bd7d4ea8c745c29
robiya07 <obidjonova07@mail.ru>
2022-12-27 18:17:22 +0500

    products 2

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from apps.models import Product
+
+
+@admin.register(Product)
+class ProductsAdmin(ModelAdmin):
+    pass
