ae4254cf64dc2996fccba7e8e3706c92cf2d934b
xolmomin <xolmomin@gmail.com>
2022-12-27 16:58:38 +0500

    change python file

--- test_python.py
+++ test_python.py
@@ -1,7 +1,3 @@
-
-
-
-
 # robiya toq sonlar fun_odd
 # akmalga 1-n gacha sonlarni print fun_print
 # hushnud ngacha bolgan sonlarni teskari chiqarsin
@@ -11,6 +7,7 @@
 def teskari(n):
     return [i for i in range(n)][::-1]
 
+
 def fun_print(n):
     return [i for i in range(1, n + 1)]
 
@@ -18,11 +15,13 @@ def fun_print(n):
 def fun_odd(nums):
     return [i for i in nums if i & 1]
 
+
 def fun_even(n):
     if n % 2:
         return 'Juft Son'
     else:
         'Juft Son'
+
 
 def from_one_to_n(n):
     return [i for i in range(1, n + 1)] * 2
