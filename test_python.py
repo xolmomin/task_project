4b07ea6dc0a7f18a57fd2d4f2b86c664388a3b25
XushnudRavshanjonov <xushnudravshanjonov23@gmail.com>
2022-12-27 16:56:31 +0500

    add reverse function

--- test_python.py
+++ test_python.py
@@ -5,6 +5,8 @@
 # javlon 1-n gacha sonlarni 2marta chiqaridan
 # adhamjonga juft sonlar fun_even
 
+def teskari(n):
+    return [i for i in range(n)][::-1]
 
 def fun_odd(nums):
     return [i for i in nums if i & 1]
