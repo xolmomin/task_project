09bba72244e8902aa02178b66e62435166b0c097
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2022-12-27 17:45:41 +0500

    register custom-user on admin

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from apps.models import User
+
+
+@admin.register(User)
+class User(ModelAdmin):
+    list_display = ('username', 'first_name', 'email')
+    exclude = ('last_login', 'groups', 'user_permissions', 'date_joined')
--- apps/forms.py
+++ apps/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm, CharField, EmailField
 
+from apps.models import User
+
 
 class RegisterForm(ModelForm):
     first_name = CharField(max_length=50)
@@ -8,3 +10,7 @@ class RegisterForm(ModelForm):
     email = EmailField(max_length=255)
 
 
+class EditProfile(ModelForm):
+    class Meta:
+        model = User
+        fields = ('image', 'first_name', 'last_name', 'email', 'phone', 'biography')
