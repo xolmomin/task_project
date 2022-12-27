e3d3ef293997c7d7c0ab1f1058390082552a9b6d
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2022-12-27 18:12:12 +0500

    add static and media root on

--- root/urls.py
+++ root/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from root.settings import STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(STATIC_URL, document_root=STATIC_ROOT) + static(MEDIA_URL, document_root=MEDIA_ROOT)
