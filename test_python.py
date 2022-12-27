26201766e023f2b09716f06e584fa22753cbba17
xolmomin <xolmomin@gmail.com>
2022-12-27 16:42:38 +0500

    todo task

---
+++ test_python.py
@@ -0,0 +1,8 @@
+
+# robiya toq sonlar fun_odd
+# akmalga 1-n gacha sonlarni print fun_print
+# hushnud ngacha bolgan sonlarni teskari chiqarsin
+# javlon 1-n gacha sonlarni 2marta chiqaridan
+# adhamjonga juft sonlar fun_even
+
+
