3eec469f8ba9f2109ed0c4305240eb7d8831d972
Javlon Baxtiyorov <baxtiyorovjavlon8@gmail.com>
2022-12-27 18:21:19 +0500

    add static and media root on

--- apps/urls.py
+++ apps/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from apps.views import ProfileSettingView, ProductListView
 
 urlpatterns = [
     path('edit-profile', ProfileSettingView.as_view(), name='edit_profile_view'),
+    path('', include('apps.urls')),
     path('products', ProductListView.as_view(), name='products')
 ]
