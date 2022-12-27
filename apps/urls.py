247353d286f5b882222b64f0012b0c04bf4065bc
adkahmjon <adkhamjonmakmudov@gmail.com>
2022-12-27 18:20:50 +0500

    update versions

--- apps/urls.py
+++ apps/urls.py
@@ -5,8 +5,6 @@ from apps.views import ProfileSettingView, ProductListView
 
 urlpatterns = [
     path('edit-profile', ProfileSettingView.as_view(), name='edit_profile_view'),
-    path('products', ProductListView.as_view(), name='products'),
-    path('edit-profile/', ProfileSettingView.as_view(), name='edit_profile_view'),
     path('products/', ProductListView.as_view(), name='products'),
     path('login/', LoginPageView.as_view(), name='login_view'),
 
