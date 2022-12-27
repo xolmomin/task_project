5ebfdeba883c0d9c83e8512deae71c24f33c61cb
Yuldoshaliyev1 <akmalyuldoshaliev@gmail.com>
2022-12-27 18:25:55 +0500

    change nimadir

--- apps/urls.py
+++ apps/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from apps.views import ProfileSettingView, ProductListView, RegisterPage
 
 urlpatterns = [
-    path('register', RegisterPage.as_view(), name = 'register_view'),
+    path('register', RegisterPage.as_view(), name='register_view'),
     path('edit-profile/', ProfileSettingView.as_view(), name='edit_profile_view'),
     path('products/', ProductListView.as_view(), name='products')
 ]
